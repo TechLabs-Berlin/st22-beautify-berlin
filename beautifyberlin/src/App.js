8cd83d48ac8a9199c1b63d65b711dc71b9847c2a
bastiroyale <sebastian-geisser@web.de>
2022-06-26 14:58:00 +0200

    changes on header and creation of pages folders

--- beautifyberlin/src/App.js
+++ beautifyberlin/src/App.js
@@ -1,69 +1,13 @@
 import './App.css'
 import TopBar from './components/topbar/TopBar';
-import HOFCard from './components/HOFCard/HOFCard';
+import HOF from './pages/HOF/HOF';
 
 function App() {
   return (
     <div className ="head">
       <>
       <TopBar />
-      <p>Text</p>
-      <div className="Post">
-      <HOFCard 
-      title="Seumestraße, Berlin"
-      imageUrl="https://i.pinimg.com/originals/f7/1d/68/f71d68249b385b0554a52e7fbfec13d9.jpg"
-      body="This work of art was made by Ms. Mila Mila. She's one of the most famous artist's in the world."
-      />
-      </div>
-      <div className="Post">
-      <HOFCard 
-      title="Seumestraße, Berlin"
-      imageUrl="https://i.pinimg.com/originals/f7/1d/68/f71d68249b385b0554a52e7fbfec13d9.jpg"
-      body="This work of art was made by Ms. Mila Mila. She's one of the most famous artist's in the world."
-      />
-      </div>
-      <div className="Post">
-      <HOFCard 
-      title="Seumestraße, Berlin"
-      imageUrl="https://i.pinimg.com/originals/f7/1d/68/f71d68249b385b0554a52e7fbfec13d9.jpg"
-      body="This work of art was made by Ms. Mila Mila. She's one of the most famous artist's in the world."
-      />
-      </div>
-      <div className="Post">
-      <HOFCard 
-      title="Seumestraße, Berlin"
-      imageUrl="https://i.pinimg.com/originals/f7/1d/68/f71d68249b385b0554a52e7fbfec13d9.jpg"
-      body="This work of art was made by Ms. Mila Mila. She's one of the most famous artist's in the world."
-      />
-      </div>
-      <div className="Post">
-      <HOFCard 
-      title="Seumestraße, Berlin"
-      imageUrl="https://i.pinimg.com/originals/f7/1d/68/f71d68249b385b0554a52e7fbfec13d9.jpg"
-      body="This work of art was made by Ms. Mila Mila. She's one of the most famous artist's in the world."
-      />
-      </div>
-      <div className="Post">
-      <HOFCard 
-      title="Seumestraße, Berlin"
-      imageUrl="https://i.pinimg.com/originals/f7/1d/68/f71d68249b385b0554a52e7fbfec13d9.jpg"
-      body="This work of art was made by Ms. Mila Mila. She's one of the most famous artist's in the world."
-      />
-      </div>
-      <div className="Post">
-      <HOFCard 
-      title="Seumestraße, Berlin"
-      imageUrl="https://i.pinimg.com/originals/f7/1d/68/f71d68249b385b0554a52e7fbfec13d9.jpg"
-      body="This work of art was made by Ms. Mila Mila. She's one of the most famous artist's in the world."
-      />
-      </div>
-      <div className="Post">
-      <HOFCard 
-      title="Seumestraße, Berlin"
-      imageUrl="https://i.pinimg.com/originals/f7/1d/68/f71d68249b385b0554a52e7fbfec13d9.jpg"
-      body="This work of art was made by Ms. Mila Mila. She's one of the most famous artist's in the world."
-      />
-      </div>
+      <HOF />
       </>
     </div>
   );
