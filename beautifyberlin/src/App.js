ba57606524a6e1634f0bada09a6beb0e77ac3c67
bastiroyale <sebastian-geisser@web.de>
2022-06-30 21:34:23 +0200

    Implementation of mui

--- beautifyberlin/src/App.js
+++ beautifyberlin/src/App.js
@@ -1,12 +1,12 @@
 import TopBar from './components/topbar/TopBar';
-import HOF from './pages/HOF/HOF';
+import SinglePage from './components/singlepage/SinglePage'
 
 function App() {
   return (
     <div className ="head">
       <>
       <TopBar />
-      <HOF />
+      <SinglePage />
       </>
     </div>
   );
