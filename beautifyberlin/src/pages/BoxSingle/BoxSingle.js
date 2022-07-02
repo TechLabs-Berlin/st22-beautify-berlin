3cc32d74ac1578cc9bd987bb4a45344e44f2757f
sv <ste1ios@yahoo.com>
2022-07-02 18:35:57 +0300

    delete Boxsingle folder

---
+++ beautifyberlin/src/pages/BoxSingle/BoxSingle.js
@@ -0,0 +1,11 @@
+import TopBar from "../../components/topbar/TopBar";
+import "./BoxSingle.css";
+
+export default function BoxSingle() {
+  return (
+    <div className="single">
+      <TopBar />
+      box single
+    </div>
+  );
+}
