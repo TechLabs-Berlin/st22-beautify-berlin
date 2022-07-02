3a5bd489b3198471c3afd348b8eaad7692aac3ea
sv <ste1ios@yahoo.com>
2022-07-03 00:22:52 +0300

    changes

--- beautifyberlin/src/pages/BoxSingle/BoxSingle.js
+++ beautifyberlin/src/pages/BoxSingle/BoxSingle.js
@@ -2,7 +2,7 @@ import "./BoxSingle.css";
 import BoxImage from "../../components/BoxImage/BoxImage";
 import Sidebar from "../../components/SideBar/SideBar";
 import Header from "../../components/Header/Header";
-import Apply from "../../components/apply/Apply";
+import Apply from "../../components/Apply/Apply";
 
 export default function BoxSingle() {
   return (
