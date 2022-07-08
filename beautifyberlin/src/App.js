9bbf6c31d2cfcc84cc8bd04cfae5b0dc8a47dfff
bastiroyale <sebastian-geisser@web.de>
2022-07-08 23:53:29 +0200

    Im lost

--- beautifyberlin/src/App.js
+++ beautifyberlin/src/App.js
@@ -1,15 +1,45 @@
+<<<<<<< Updated upstream
+<<<<<<< Updated upstream
+<<<<<<< Updated upstream
 import TopBar from './components/topbar/TopBar';
 import NewBox from './components/newbox/NewBox';
+=======
+import TopBar from "./components/topbar/TopBar";
+import NewBox from "./pages/NewBox/NewBoxes";
+>>>>>>> Stashed changes
+=======
+import TopBar from "./components/topbar/TopBar";
+import NewBox from "./pages/NewBox/NewBoxes";
+>>>>>>> Stashed changes
+=======
+import TopBar from "./components/topbar/TopBar";
+import NewBox from "./pages/NewBox/NewBoxes";
+>>>>>>> Stashed changes
 
 function App() {
   return (
-    <div className ="head">
+    <div className="head">
       <>
+<<<<<<< Updated upstream
+<<<<<<< Updated upstream
+<<<<<<< Updated upstream
       <TopBar />
       <NewBox />
+=======
+        <TopBar />
+        <NewBox />
+>>>>>>> Stashed changes
+=======
+        <TopBar />
+        <NewBox />
+>>>>>>> Stashed changes
+=======
+        <TopBar />
+        <NewBox />
+>>>>>>> Stashed changes
       </>
     </div>
   );
 }
 
-export default App
+export default App;
