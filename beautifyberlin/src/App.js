8799e43ab6c289df33d7084dc78bcd40f3e5ec1f
bastiroyale <sebastian-geisser@web.de>
2022-07-09 00:14:35 +0200

    ok

--- beautifyberlin/src/App.js
+++ beautifyberlin/src/App.js
@@ -1,22 +1,12 @@
-<<<<<<< Updated upstream
-import TopBar from './components/topbar/TopBar';
-import NewBox from './components/newbox/NewBox';
-=======
 import TopBar from "./components/topbar/TopBar";
 import NewBox from "./components/newbox/NewBox";
->>>>>>> Stashed changes
 
 function App() {
   return (
     <div className="head">
       <>
-<<<<<<< Updated upstream
-      <TopBar />
-      <NewBox />
-=======
         <TopBar />
         <NewBox />
->>>>>>> Stashed changes
       </>
     </div>
   );
