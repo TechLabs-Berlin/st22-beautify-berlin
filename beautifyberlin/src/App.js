1606067c687193446f5c7f6f5459ba3fb6c2e80d
bastiroyale <sebastian-geisser@web.de>
2022-07-15 19:02:15 +0200

    changes on color of statuschange buttons

--- beautifyberlin/src/App.js
+++ beautifyberlin/src/App.js
@@ -1,12 +1,12 @@
 import TopBar from "./components/topbar/TopBar";
-import AboutUs from "./pages/AboutUs/AboutUs";
+import Changestatus from "./pages/ChangeStatus/Changestatus";
 
 function App() {
   return (
     <div className="head">
       <>
         <TopBar />
-        <AboutUs />
+        <Changestatus />
       </>
     </div>
   );
